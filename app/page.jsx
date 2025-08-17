6cb0c53b1c88bf8b70d10134c55b0f577125d6e5
Shahriar Ratul <ratul3123@gmail.com>
2025-08-17 20:30:35 +0600

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Our Website</h1>
+      <p>
+        We are glad to have you here. Explore our content and enjoy your stay!
+      </p>
+    </div>
+  );
+}
