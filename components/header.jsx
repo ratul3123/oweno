6a7e13b933d901ae8bc51709c0980e921d67ba36
Shahriar Ratul <ratul3123@gmail.com>
2025-08-18 14:34:06 +0600

    store authenticated user in db

--- components/header.jsx
+++ components/header.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+"use client";
+
+import useStoreUser from "@/hooks/use-store-user";
 import {SignedIn, SignedOut, UserButton, SignInButton, SignUpButton} from "@clerk/nextjs";
+import React from "react";
 
 const Header = () => {
+
+    const { isloading } = useStoreUser();
+
     return ( 
-        <div>
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-        </div>
+        <header className="fixed top-0 w-full border-b bg-white/95 backdrop-blur z-50 supports-[backdrop-filter]:bg-white/60">
+            <nav>
+                <SignedOut>
+                  <SignInButton />
+                  <SignUpButton />
+                </SignedOut>
+                <SignedIn>
+                  <UserButton />
+                </SignedIn>
+            </nav>
+
+            { isloading && <BarLoader width={"100%"} color="#36d7b7" /> }
+
+        </header>
         );
 };
 
