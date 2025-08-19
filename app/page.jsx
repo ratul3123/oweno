46e27809efea9f36ab76de921248870eff4e4b0d
Shahriar Ratul <ratul3123@gmail.com>
2025-08-19 11:33:11 +0600

    build header component with user profile

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div>
+    <div className="pt-16">
       <h1>Welcome to Our Website</h1>
       <p>
         We are glad to have you here. Explore our content and enjoy your stay!
