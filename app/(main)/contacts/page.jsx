1df6c7cfa4f7807940bd1d0398425a3b396e671a
Shahriar Ratul <ratul3123@gmail.com>
2025-08-20 22:50:43 +0600

    create contacts page

---
+++ app/(main)/contacts/page.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const ContactsPage = () => {
+    return <div>ContactsPage</div>
+};
+
+export default ContactsPage;
---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DashboardPage = ({ children }) => {
+    return <div>DashboardPage</div>
+};
+
+export default DashboardPage;
