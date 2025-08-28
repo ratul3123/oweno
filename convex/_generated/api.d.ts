85225f5bb1afa59ddf15f63a8a92af39ce49c0d6
Shahriar Ratul <ratul3123@gmail.com>
2025-08-29 00:34:51 +0600

    rename innjest.js to inngest.js (typo)

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -18,7 +18,7 @@ import type * as dashboard from "../dashboard.js";
 import type * as email from "../email.js";
 import type * as expenses from "../expenses.js";
 import type * as groups from "../groups.js";
-import type * as innjest from "../innjest.js";
+import type * as inngest from "../inngest.js";
 import type * as seed from "../seed.js";
 import type * as users from "../users.js";
 
@@ -36,7 +36,7 @@ declare const fullApi: ApiFromModules<{
   email: typeof email;
   expenses: typeof expenses;
   groups: typeof groups;
-  innjest: typeof innjest;
+  inngest: typeof inngest;
   seed: typeof seed;
   users: typeof users;
 }>;
