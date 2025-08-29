019d022910d8264712f90c4b585153f2e796c9fe
Shahriar Ratul <ratul3123@gmail.com>
2025-08-30 00:36:15 +0600

    build APIs for expense creation

--- convex/groups.js
+++ convex/groups.js
@@ -185,3 +185,77 @@ export const getGroupExpenses = query({
     };
   },
 });
+
+export const getGroupOrMembers = query({
+  args: {
+    groupId: v.optional(v.id("groups")), // Optional - if provided, will return details for just this group
+  },
+  handler: async (ctx, args) => {
+    // Use centralized getCurrentUser function
+    const currentUser = await ctx.runQuery(internal.users.getCurrentUser);
+
+    // Get all groups where the user is a member
+    const allGroups = await ctx.db.query("groups").collect();
+    const userGroups = allGroups.filter((group) =>
+      group.members.some((member) => member.userId === currentUser._id)
+    );
+
+    // If a specific group ID is provided, only return details for that group
+    if (args.groupId) {
+      const selectedGroup = userGroups.find(
+        (group) => group._id === args.groupId
+      );
+
+      if (!selectedGroup) {
+        throw new Error("Group not found or you're not a member");
+      }
+
+      // Get all user details for this group's members
+      const memberDetails = await Promise.all(
+        selectedGroup.members.map(async (member) => {
+          const user = await ctx.db.get(member.userId);
+          if (!user) return null;
+
+          return {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            imageUrl: user.imageUrl,
+            role: member.role,
+          };
+        })
+      );
+
+      // Filter out any null values (in case a user was deleted)
+      const validMembers = memberDetails.filter((member) => member !== null);
+
+      // Return selected group with member details
+      return {
+        selectedGroup: {
+          id: selectedGroup._id,
+          name: selectedGroup.name,
+          description: selectedGroup.description,
+          createdBy: selectedGroup.createdBy,
+          members: validMembers,
+        },
+        groups: userGroups.map((group) => ({
+          id: group._id,
+          name: group.name,
+          description: group.description,
+          memberCount: group.members.length,
+        })),
+      };
+    } else {
+      // Just return the list of groups without member details
+      return {
+        selectedGroup: null,
+        groups: userGroups.map((group) => ({
+          id: group._id,
+          name: group.name,
+          description: group.description,
+          memberCount: group.members.length,
+        })),
+      };
+    }
+  },
+});
